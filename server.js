ad4d51e481bd8f35b5652cb3c41beed2a94363f6
ivankkdgtl <ivan@kokadigital.com>
2019-09-19 19:18:21 +0200

    asd

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.korisnici) })
+app.get('/', (req, res)=> { res.send('gg') })
 app.post('/prijava', prijava.prijavi(db, bcrypt))
 app.post('/registracija', (req, res) => { registracija.registruj(req, res, db, bcrypt) })
 app.put('/slika', (req, res) => { slika.brUpita(req, res, db)})
